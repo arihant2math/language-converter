ca4569db0b3eb291307dbc137e17251443d5f770
Ashwin <58895652+arihant2math@users.noreply.github.com>
2021-06-18 14:54:40 -0700

    Update detect.py

--- detect.py
+++ detect.py
@@ -1,7 +1,9 @@
+"""Finds the language by file extention"""
+
 
 def get_lang(file_extention):
     """
     :param file:
     """
-    lang_map = {".c": "c", "c++": "c++", "c#": "c#", ".py": "python", ".java": "java", ".ruby": "ruby", ".r": "r"}
-    return lang_map.get(file_extention)
+    lang_map = {".c": "c", "c++": "c++", "c#": "c#", ".py": "python", ".java": "java", ".ruby": "ruby", ".r": "r", ".go", "go"}
+    return lang_map[file_extention]
