e522cf1aafca57d693c8fca0ca09633820ad2b5b
Ashwin <58895652+arihant2math@users.noreply.github.com>
2021-06-18 15:04:32 -0700

    Update convert.py

--- convert.py
+++ convert.py
@@ -1,8 +1,1 @@
-def from_python(file):
-    """
-    coverts to base lang from python
-    """
-    file = str(file).replace("    ", "\t")
-    lines = file.split("\n")
-    lines = from_python_r(lines)
-    return lines
+
